dcbdc1c5337d40228cf9df5762a6de1c3c61a361
anytimesoon <damien@delartigue.co.uk>
2023-10-30 11:26:51 +0100

    sql clean up

--- db/init.sql
+++ db/init.sql
@@ -1,5 +1,2 @@
 GRANT ALL PRIVILEGES ON *.* TO 'eurovision'@'%' WITH GRANT OPTION;
--- GRANT SUPER ON *.* TO 'eurovision'@'localhost';
-
--- UPDATE mysql.user SET Super_Priv='Y' WHERE user='eurovision' AND host='%';
 FLUSH PRIVILEGES;
